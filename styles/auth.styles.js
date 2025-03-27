a6987aa4fa3023e80b612e77e71bebf7d5d4001a
devloper-jp <jnlpansuriya@gmail.com>
2025-03-27 13:22:45 -0400

    commit my first

---
+++ styles/auth.styles.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+export const styles = StyleSheet.create({
+    container : {
+        flex : 1,
+        alignitems : "center",
+        justifycontent :"center",
+    },
+    title : {
+        color : "red",
+        fontsize : 20,
+    },
+})
